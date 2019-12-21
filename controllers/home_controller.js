3640980d8f56f8a2a005bc9803866cbbd7979db6
Ayush Kumar Singh <astroash13.1@gmail.com>
2019-12-21 17:20:04 +0530

    Add a posts section in home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,12 @@
-module.exports.home = function(req, res) {
-    console.log(req.cookies);
-    res.cookie('user_id', 35);
-    return res.render('home', {
-        title: "Home"
+const Post = require('../models/post');
+
+module.exports.home = function(req, res) { 
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Home",
+            post: posts
+        });
     });
+
 }
