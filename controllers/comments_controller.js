51858551f130f8af22e9b35c2e11fcff8883f43a
Ayush Kumar Singh <astroash13.1@gmail.com>
2019-12-24 16:02:41 +0530

    Corrected the layout

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -30,20 +30,20 @@ module.exports.create = function(req, res) {
 
 module.exports.destroy = function(req, res) {
     Comment.findById(req.params.id, function(err, comment) {
-        if(comment.user == req.user.id) {
-            let postId = comment.post;
 
-            console.log(postId);
+        let postId = comment.post;
 
-            comment.remove();
-            
-            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post) {
-                console.log("I am trying to pull!!"); 
+        Post.findById(postId, function(err, post) {
+            if(comment.user == req.user.id || post.user == req.user.id) {
+
+                comment.remove();
+                
+                Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post) {
+                    return res.redirect('back');
+                });
+            } else {
                 return res.redirect('back');
-            });
-            console.log("I am still here!!"); 
-        } else {
-            return res.redirect('back');
-        }
+            } 
+        });
     })
 }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,22 +1,27 @@
 const Post = require('../models/posts');
 const User = require('../models/users');
 
-module.exports.home = function(req, res) { 
+module.exports.home = async function(req, res) { 
 
-    Post.find({}).populate('user').populate({
-        path: "comments",
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+    try {
 
-        User.find({}, function(err, user) {
-            return res.render('home', {
-                title: "Home",
-                post: posts,
-                all_users: user
-            });
+        let posts = await Post.find({}).populate('user').populate({
+            path: "comments",
+            populate: {
+                path: 'user'
+            }
         });
-    });
+        
+        let users = await User.find({});
+             
+        return res.render('home', {
+            title: "Home",
+            post: posts,
+            all_users: users
+        });
+
+    } catch (err) {
+        console.log(err);
+        return;
+    }
 }
