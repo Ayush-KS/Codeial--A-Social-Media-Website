4593e52cc75e7f6bb87e1c46a401aa59ebdcf425
Ayush Kumar Singh <astroash13.1@gmail.com>
2020-01-07 22:58:06 +0530

    Add Authentication using JWT

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,39 @@
-module.exports.index = function(req, res) {
+const Post = require('../../../models/posts');
+const Comment = require('../../../models/comments');
+
+module.exports.index = async function(req, res) {
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user', '-password')
+        .populate({
+            path: "comments",
+            populate: {
+                path: 'user',
+                select: '-password'
+            }
+        });
+
     return res.json(200, {
         message: "List of posts",
-        posts: []
-    })
+        posts: posts
+    });
+}
+
+module.exports.destroy = async function(req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
+        post.remove();
+        await Comment.deleteMany({
+            post: req.params.id
+        });
+
+        return res.json(200, {
+            message: "Post and associated comments deleted!"
+        })
+    } catch {
+        return res.json(500, {
+            message: "Internal Server Error"
+        });
+    }
 }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 
 //Setting up MongoStore
 const MongoStore = require('connect-mongo')(session);
